19924df59c58dd72848277dab4a282ff42adb631
Ibrahim Maâzou Djahadi <ibrahimdjahadi46@gmail.com>
2025-11-21 10:05:57 +0200

    fix(auth): added better-auth bearer plugin

--- backend/src/better-auth.config.ts
+++ backend/src/better-auth.config.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from 'better-auth';
 import { PrismaClient } from 'generated/prisma/client';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-
+import { bearer } from 'better-auth/plugins';
 const MAX_PASSWORD_LENGTH = 21;
 const MIN_PASSWORD_LENGTH = 8;
 
 const prismaClient = new PrismaClient();
 export const betterAuthConfig = betterAuth({
+  plugins: [bearer()],
   user: {
     additionalFields: {
       role: { type: 'string', defaultValue: 'teacher', input: false },
